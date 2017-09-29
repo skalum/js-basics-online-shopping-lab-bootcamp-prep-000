0465f9c81d1b08fa25d5f83a9838f7a180b3095a
skalum <stkalum@gmail.com>
2017-09-29 20:54:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function viewCart() {
   const itemsAndPrices = [];
 
   for (var i = 0; i < (cart.length - 1); i++) {
-    const item = [[cart[i]].keys()][0];
+    const item = [[cart[i]].keys()
 
     console.log(item);
 
