b72b02605ccfbeb4fa53da968fa975dcd9d49a84
skalum <stkalum@gmail.com>
2017-09-29 21:55:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,10 +73,9 @@ function removeFromCart(itemName) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber !== null) {
-    const cost = total();
+  if (cardNumber != null) {
 
-    console.log(`Your total cost is ${cost}, which will be charged to the card ${cardNumber}.`);
+    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
     cart = [];
   } else {
     console.log("Sorry, we don't have a credit card on file for you.");
