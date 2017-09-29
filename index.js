58623ea985347a4760e11cf940bd0f2e5bd695f3
skalum <stkalum@gmail.com>
2017-09-29 21:51:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function removeFromCart(itemName) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber != NULL) {
+  if (cardNumber != null) {
     console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
     cart = [];
   } else {
