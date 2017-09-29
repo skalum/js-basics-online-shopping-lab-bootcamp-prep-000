1c90db2137863e95b097ec49fdaa702565be427a
skalum <stkalum@gmail.com>
2017-09-29 21:54:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function placeOrder(cardNumber) {
   if (cardNumber != null) {
     const cost = total();
 
-    console.log(`Your total cost is ${total}, which will be charged to the card ${cardNumber}.`);
+    console.log(`Your total cost is ${cost}, which will be charged to the card ${cardNumber}.`);
     cart = [];
   } else {
     console.log("Sorry, we don't have a credit card on file for you.");
