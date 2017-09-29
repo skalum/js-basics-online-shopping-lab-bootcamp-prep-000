d72e544df98c5178c47593077cf2f60c24f2f576
skalum <stkalum@gmail.com>
2017-09-29 21:48:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,7 +73,12 @@ function removeFromCart(itemName) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
+  } else {
+    console.log("Sorry, we don't have a credit card on file for you.");
+  }
 }
 
 function getRandomInt(min, max) {
